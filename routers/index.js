ab74f53bb697e806826f5af6d625adc23f1d2cc7
IvanZaborskih <ivan.31.03@mail.ru>
2022-02-03 17:01:56 +0300

    update models

--- routers/index.js
+++ routers/index.js
@@ -1,7 +1,15 @@
 const Router = require("express");
 const router = new Router();
 const userRouter = require("./userRouter");
+const tagRouter = require('./tagRouter');
+const groupRouter = require('./groupRouter');
+const categoryRouter = require('./categoryRouter');
+const taskRouter = require('./taskRouter');
 
 router.use('/user', userRouter);
+router.use('/tag', tagRouter);
+router.use('/group', groupRouter);
+router.use('/category', categoryRouter);
+router.use('/task', taskRouter);
 
 module.exports = router;
